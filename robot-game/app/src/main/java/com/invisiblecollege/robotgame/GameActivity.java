fb9b438d9427863c98d65b607fd292a790231512
Jason Studstill <jason@jasonstudstill.com>
2016-02-22 19:06:21 -0800

    First part of class code

--- robot-game/app/src/main/java/com/invisiblecollege/robotgame/GameActivity.java
+++ robot-game/app/src/main/java/com/invisiblecollege/robotgame/GameActivity.java
@@ -24,15 +24,60 @@ import java.util.ArrayList;
 /**
  * Created by randy.thedford on 11/27/15.
  */
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements CustomView.CustomViewEvents, View.OnTouchListener {
 
+    CustomView mGameView;
 
+    RectF mScreenRect = new RectF();
+
+    int mLoops = 0;
+    int mScore = 0;
+
+    Paint mPaint;
+    Paint mFloorPaint;
+
+    AnimatedSprite mRobot;
+
+    long mTimer = 0;
+    long mMaxTimer = 60000;
+
+    SoundPool mSoundPool;
+
+    int mHitSound;
+    int mMissSound;
+    int mGameOverSound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        mGameView = (CustomView) findViewById(R.id.game_view);
+        mGameView.setCustomViewListener(this);
+        mGameView.setOnTouchListener(this);
+
+        mPaint = new Paint();
+        mPaint.setColor(Color.BLACK);
+        mPaint.setTextSize(50);
+
+        mFloorPaint = new Paint();
+        Bitmap floorBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.floor);
+        BitmapShader shader = new BitmapShader(floorBitmap, Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+
     }
 
+    @Override
+    public void onUpdate(long elapsedTime) {
+
+    }
+
+    @Override
+    public void onDraw(Canvas c) {
+
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return false;
+    }
 }
